cf5f455ef730e98b23b8f77f13744870dd3caee2
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-07-22 01:21:18 +0900

    part1 基本サンプル追加

---
+++ 03_mysql_sample/mysql/init.sql
@@ -0,0 +1,4 @@
+CREATE USER 'exporter'@'%' IDENTIFIED BY 'password';
+GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
+GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
+FLUSH PRIVILEGES;
